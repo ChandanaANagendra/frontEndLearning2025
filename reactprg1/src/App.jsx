f1307f0b6835a98eb758487aa9af97938e165359
ChandanaANagendra <chanduammuchandu3030@gmail.com>
2025-10-16 10:35:16 +0530

    Add files via upload
    react

---
+++ reactprg1/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return(
+    <>
+    <Header />{" "}
+    <div>
+      <h1>Hello From React</h1>
+      <p>I am Chandana, From AIML</p>
+
+    </div>
+    </>
+  );
+}
+export default App;           
---
+++ reactprg1/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>This is my header</h1>
+        </header>
+    )
+}
+export default Header;
